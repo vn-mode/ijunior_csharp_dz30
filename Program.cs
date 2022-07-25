d85efa45c7d760664a3ae3e2d405e22cfa6d485b
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-07-25 04:49:52 +0300

    dz_30
    dz_30

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            bool isOpen = true;
+
+
+            while (isOpen)
+            {
+                Console.Write("Введите целое число: ");
+                RequestInt(Console.ReadLine(), out userNumber);
+                if (userNumber != 0)
+                {
+                    isOpen = false;
+                }
+            }
+
+            static void RequestInt(string message, out int userNumber)
+            {
+                bool result = int.TryParse(message, out userNumber);
+                if (result)
+                {
+                    Console.WriteLine("Отлично! Вы ввели " + userNumber);
+                }
+                else
+                {
+                    Console.WriteLine("Не удалось!");
+                }
+            }
+        }
+    }
+}
+
